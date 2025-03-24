a8596f409e6e6c49ecafbe5e8b21a98324c68c1e
MarcoNappa <marcomarconappa@gmail.com>
2025-03-24 12:31:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+alert("Io sono un new Web Dev :)");
+
+prompt("Mi raccomando scrivi 'permesso' per entrare, educazione prima di tutto");
+
+var saluto = ("Entra pure amico/a mio/a");
+
+alert(saluto);
+
+var saluto ="bravo/a ed eductao/a";
+
+alert(saluto);
+
+var nomeutente = prompt("Come ti chiami?");
+var messaggio = "Ciao " + nomeutente + " benvenuto nella mia pagina Portfolio";
+document.write (messaggio);
